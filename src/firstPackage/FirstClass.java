793259a341056928d86429d4f19326e9f3804f43
Anriverd <114427071+Anriverd@users.noreply.github.com>
2023-01-09 11:44:13 +0600

    Update FirstClass.java

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,5 +3,8 @@ package firstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        
+        System.out.println("I made it on the remote repository");
     }
+    
 }
